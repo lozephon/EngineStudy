fe8a9204feaf48e92a8be01a53401a2a189b5908
lozephon <lozephon@gmail.com>
2015-10-17 03:26:15 +0900

    윈도우 여러개 만들어보고 GDI 래퍼 소스파일 추가

--- LApp/main.cpp
+++ LApp/main.cpp
@@ -1,8 +1,10 @@
 #include <Windows.h>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc2(HWND, UINT, WPARAM, LPARAM);
 HINSTANCE g_hInst;
 LPTSTR lpszClass = TEXT("First");
+LPTSTR lpszClass2 = TEXT("Second");
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
 	, LPSTR lpszCmdParam, int nCmdShow)
@@ -29,16 +31,49 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
 		NULL, (HMENU)NULL, hInstance, NULL);
 	ShowWindow(hWnd, nCmdShow);
 
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc2;
+	WndClass.lpszClassName = lpszClass2;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	HWND hWnd2 = CreateWindow(lpszClass2, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd2, nCmdShow);
+
 	while (GetMessage(&Message, 0, 0, 0)) {
 		TranslateMessage(&Message);
 		DispatchMessage(&Message);
 	}
-	return Message.wParam;
+
+	UnregisterClass(WndClass.lpszClassName, hInstance);
+	return 0;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	switch (iMessage) {
+	case WM_LBUTTONDOWN:
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+LRESULT CALLBACK WndProc2(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMessage) {
+	case WM_LBUTTONDOWN:
+		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
---
+++ LEngine/LBuild.h
@@ -0,0 +1,7 @@
+#pragma once
+enum EGDIType
+{
+	EGDIType_DX9,
+	EGDIType_OpenGL,
+	EGDIType_DX11
+};
