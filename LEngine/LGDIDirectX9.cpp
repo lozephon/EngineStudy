539f01bd74f7a1d7ac53eea4b11f3f779e089b2b
lozephon <lozephon@gmail.com>
2016-03-29 00:37:24 +0900

    3D Game Programming Project 추가

--- LEngine/LGDIDirectX9.cpp
+++ LEngine/LGDIDirectX9.cpp
@@ -1,5 +1,6 @@
 #include"LGDIDirectX9.h"
 #include"LMath.h"
+#include<stdio.h>
 
 LGDIDirectX9::LGDIDirectX9(HWND hWnd, LGDI_Type type) :
 	LGDIWrapper(hWnd, type),
@@ -7,7 +8,8 @@ LGDIDirectX9::LGDIDirectX9(HWND hWnd, LGDI_Type type) :
 	m_pD3DDevice(NULL)
 {
 	initDevice();
-	initVB();
+	initGeometry();
+	initIB();
 }
 
 LGDIDirectX9::~LGDIDirectX9()
@@ -28,6 +30,8 @@ bool LGDIDirectX9::initDevice()
 // TODO 윈도우 모드와 전체화면 모드 수정 가능하게
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
 	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	d3dpp.EnableAutoDepthStencil = TRUE;
+	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
 
 	if (FAILED(m_pD3D->CreateDevice(D3DADAPTER_DEFAULT,
 		D3DDEVTYPE_HAL,
@@ -37,31 +41,102 @@ bool LGDIDirectX9::initDevice()
 		return 0;
 
 	m_pD3DDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+	m_pD3DDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
+	//m_pD3DDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
 	m_pD3DDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
 
 	return 1;
 }
 
-bool LGDIDirectX9::initVB()
+bool LGDIDirectX9::initGeometry()
 {
-	VERTEX1 vertices[] =
-	{
-		{-1.0f, -1.0f, 0.0f, 0xffff0000, },
-		{1.0f, -1.0f, 0.0f, 0xff0000ff, },
-		{0.0f, 1.0f, 0.0f, 0xffffffff, },
-	};
+	LPD3DXBUFFER pMtrlBuffer;
 
-	if (FAILED(m_pD3DDevice->CreateVertexBuffer(3 * sizeof(VERTEX1), 0, D3DFVF_VERTEX1, D3DPOOL_DEFAULT, &m_pVB, NULL)))
+	if (FAILED(D3DXLoadMeshFromX("..\\Resources\\Tiger.x", D3DXMESH_SYSTEMMEM, m_pD3DDevice, NULL, &pMtrlBuffer, NULL, &m_dwNumMaterials, &m_pMesh)))
+		return false;
+
+	D3DXMATERIAL* d3dxMaterials = (D3DXMATERIAL*)pMtrlBuffer->GetBufferPointer();
+
+	m_pMeshMaterials = new D3DMATERIAL9[m_dwNumMaterials];
+	m_pMeshTextures = new LPDIRECT3DTEXTURE9[m_dwNumMaterials];
+
+	for (DWORD i = 0; i < m_dwNumMaterials;i++)
+	{
+		m_pMeshMaterials[i] = d3dxMaterials[i].MatD3D;
+
+		m_pMeshMaterials[i].Ambient = m_pMeshMaterials[i].Diffuse;
+
+		m_pMeshTextures[i] = NULL;
+		if (d3dxMaterials[i].pTextureFilename != NULL && lstrlenA(d3dxMaterials[i].pTextureFilename) > 0)
+		{
+			const CHAR* prefix = "..\\Resources\\";
+			CHAR texPath[MAX_PATH];
+			sprintf_s(texPath, "..\\Resources\\%s", d3dxMaterials[i].pTextureFilename);
+			if (FAILED(D3DXCreateTextureFromFileA(m_pD3DDevice, texPath, &m_pMeshTextures[i])))
+				return false;
+		}
+	}
+
+	pMtrlBuffer->Release();
+
+	return true;
+
+	/*
+	if (FAILED(D3DXCreateTextureFromFile(m_pD3DDevice, "..\\Resources\\banana.bmp", &m_pTexture)))
+	{
+		return false;
+	}
+	
+	if (FAILED(m_pD3DDevice->CreateVertexBuffer(50 * 2 * sizeof(VERTEX1), 0, D3DFVF_VERTEX1, D3DPOOL_DEFAULT, &m_pVB, NULL)))
 	{
 		return false;
 	}
 
-	VOID* pVertices;
-	if (FAILED(m_pVB->Lock(0, sizeof(vertices), (void**)&pVertices, 0)))
+	VERTEX1* pVertices;
+	if (FAILED(m_pVB->Lock(0, 0, (void**)&pVertices, 0)))
 		return false;
 
-	memcpy(pVertices, vertices, sizeof(vertices));
+	for (DWORD i = 0;i < 50;i++)
+	{
+		FLOAT theta = (2 * D3DX_PI*i) / (50 - 1);
+		pVertices[2 * i + 0].position = D3DXVECTOR3(sinf(theta), -1.0f, cosf(theta));
+		pVertices[2 * i + 0].color = 0x00000000;
+		pVertices[2 * i + 0].tu = ((FLOAT)i) / (50 - 1);
+		pVertices[2 * i + 0].tv = 1.0f;
+
+		pVertices[2 * i + 1].position = D3DXVECTOR3(sinf(theta), 1.0f, cosf(theta));
+		pVertices[2 * i + 1].color = 0xff808080;
+		pVertices[2 * i + 1].tu = ((FLOAT)i) / (50 - 1);
+		pVertices[2 * i + 1].tv = 0.0f;
+	}
+
 	m_pVB->Unlock();
+
+	return true;
+	*/
+}
+
+bool LGDIDirectX9::initIB()
+{
+	INDEX1 indices[] = 
+	{
+		{0,1,2}, {0,2,3},
+		{4,6,5}, {4,7,6},
+		{0,3,7}, {0,7,4},
+		{1,5,6}, {1,6,2},
+		{3,2,6}, {3,6,7},
+		{0,4,5}, {0,5,1}
+	};
+
+	if (FAILED(m_pD3DDevice->CreateIndexBuffer(12 * sizeof(INDEX1), 0, D3DFMT_INDEX16, D3DPOOL_DEFAULT, &m_pIB, NULL)))
+		return false;
+
+	VOID* pIndices;
+	if (FAILED(m_pIB->Lock(0, sizeof(indices), (void**)&pIndices, 0)))
+		return false;
+
+	memcpy(pIndices, indices, sizeof(indices));
+	m_pIB->Unlock();
 
 	return true;
 }
@@ -69,10 +144,11 @@ bool LGDIDirectX9::initVB()
 void LGDIDirectX9::setupMatrices()
 {
 	D3DXMATRIXA16 matWorld;
-	UINT iTime = timeGetTime() % 1000;
-	FLOAT fAngle = iTime * (2.0f * D3DX_PI) / 1000.0f;
-	D3DXMatrixRotationY(&matWorld, fAngle);
-	m_pD3DDevice->SetTransform(D3DTS_WORLD, &matWorld);
+	D3DXMatrixIdentity(&matWorld);
+	D3DXMatrixRotationX(&matWorld, timeGetTime()/ 500.0f);
+	D3DXMATRIXA16 mat2;
+	D3DXMatrixRotationY(&mat2, timeGetTime() / 500.0f);
+	m_pD3DDevice->SetTransform(D3DTS_WORLD, &(matWorld * mat2));
 
 	D3DXVECTOR3 vEyePt(0.0f, 3.0f, -5.0f);
 	D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);
@@ -86,17 +162,62 @@ void LGDIDirectX9::setupMatrices()
 	m_pD3DDevice->SetTransform(D3DTS_PROJECTION, &matProj);
 }
 
+void LGDIDirectX9::setupLights()
+{
+	D3DMATERIAL9 mtrl;
+	ZeroMemory(&mtrl, sizeof(D3DMATERIAL9));
+
+	mtrl.Diffuse.r = mtrl.Ambient.r = 1.0f;
+	mtrl.Diffuse.g = mtrl.Ambient.g = 1.0f;
+	mtrl.Diffuse.b = mtrl.Ambient.b = 0.0f;
+	mtrl.Diffuse.a = mtrl.Ambient.a = 1.0f;
+	m_pD3DDevice->SetMaterial(&mtrl);
+
+	D3DXVECTOR3 vecDir;
+	D3DLIGHT9 light;
+	ZeroMemory(&light, sizeof(D3DLIGHT9));
+	light.Type = D3DLIGHT_DIRECTIONAL;
+
+	light.Diffuse.r = 1.0f;
+	light.Diffuse.g = 1.0f;
+	light.Diffuse.b = 1.0f;
+	vecDir = D3DXVECTOR3(cosf(timeGetTime() / 350.0f), 1.0f, sinf(timeGetTime() / 350.0f));
+	D3DXVec3Normalize((D3DXVECTOR3*)&light.Direction, &vecDir);
+	light.Range = 1000.0f;
+	m_pD3DDevice->SetLight(0, &light);
+	m_pD3DDevice->LightEnable(0, TRUE);
+	m_pD3DDevice->SetRenderState(D3DRS_LIGHTING, TRUE);
+	m_pD3DDevice->SetRenderState(D3DRS_AMBIENT, 0x00202020);
+}
+
 bool LGDIDirectX9::render()
 {
-	m_pD3DDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+	m_pD3DDevice->Clear(0, NULL, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
 
 	if (SUCCEEDED(m_pD3DDevice->BeginScene()))
 	{
+		//setupLights();
 		setupMatrices();
 
-		m_pD3DDevice->SetStreamSource(0, m_pVB, 0, sizeof(VERTEX1));
-		m_pD3DDevice->SetFVF(D3DFVF_VERTEX1);
-		m_pD3DDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+		for (DWORD i = 0;i < m_dwNumMaterials;i++)
+		{
+			m_pD3DDevice->SetMaterial(&m_pMeshMaterials[i]);
+			m_pD3DDevice->SetTexture(0, m_pMeshTextures[i]);
+
+			m_pMesh->DrawSubset(i);
+		}
+
+// 		m_pD3DDevice->SetTexture(0, m_pTexture);
+// 		m_pD3DDevice->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
+// 		m_pD3DDevice->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
+// 		m_pD3DDevice->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_DIFFUSE);
+// 		m_pD3DDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_DISABLE);
+
+ 		m_pD3DDevice->SetStreamSource(0, m_pVB, 0, sizeof(VERTEX1));
+ 		m_pD3DDevice->SetFVF(D3DFVF_VERTEX1);
+		m_pD3DDevice->SetIndices(m_pIB);
+		m_pD3DDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 8, 0, 12);
+ 		//m_pD3DDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2 * 50 -2 );
 
 		m_pD3DDevice->EndScene();
 	}
--- LEngine/LGDIDirectX9.h
+++ LEngine/LGDIDirectX9.h
@@ -13,19 +13,35 @@ public:
 
 private:
 	bool initDevice();
-	bool initVB();
+	bool initGeometry();
+	bool initIB();
+	
 	void setupMatrices();
+	void setupLights();
 
 	LPDIRECT3D9	m_pD3D;
 	LPDIRECT3DDEVICE9 m_pD3DDevice;
 
 	struct VERTEX1
 	{
-		FLOAT x, y, z;
-		DWORD color;
+		D3DXVECTOR3 position;
+		D3DCOLOR color;
+		//FLOAT	tu, tv;
 	};
 
 #define D3DFVF_VERTEX1 (D3DFVF_XYZ | D3DFVF_DIFFUSE)
 
 	IDirect3DVertexBuffer9* m_pVB;
+	LPDIRECT3DTEXTURE9		m_pTexture;
+
+	IDirect3DIndexBuffer9*	m_pIB;
+	struct INDEX1
+	{
+		WORD _0, _1, _2;
+	};
+
+	LPD3DXMESH				m_pMesh;
+	D3DMATERIAL9*			m_pMeshMaterials;
+	LPDIRECT3DTEXTURE9*		m_pMeshTextures;
+	DWORD					m_dwNumMaterials;
 };
